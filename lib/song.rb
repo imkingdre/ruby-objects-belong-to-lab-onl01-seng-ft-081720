d9edf2a103be603b814d13472f25291f552f667f
Andre King <andreking1031@gmail.com>
2020-08-31 16:21:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist
   
-  def initialize(nam, artist, title)
+  def initialize(name, artist, title)
     @name= name
     @artist = artist
     @title = title
